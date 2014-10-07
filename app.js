815b9c394eb4391514f0d7b39c55544b35baf06c
Ben Evans <bevans@zendesk.com>
2014-10-06 19:34:53 -0500

    Fully working merging
    -Select any number of users in a set
    -Merge them into a selected user
    -Testing/QA needed but it is working

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
     return {
         events: {
             'app.activated':'init',
-            'dataLoaded':'render'
+            'dataLoaded':'render',
+            'click .status-toggle': 'viewMerge',
+            'click .merge-into': 'selectUsers',
+            'click .merge-cancel': 'cancel', 
         },
 
         requests: {
@@ -17,7 +20,7 @@
             sharingAgreements: {
                 url: '/api/v2/sharing_agreements.json'
             },
-            
+
             users: {
                 url: '/api/v2/users.json'
             },
@@ -26,10 +29,77 @@
                 return {
                     'url': helpers.fmt('/api/v2/users/%@/tickets/requested.json', userID)
                 };
-            }
+            },
+
+            mergeUsers: function(winnerID, mergedID) {
+                return {
+                    url: helpers.fmt('/api/v2/users/%@/merge.json', mergedID),
+                    dataType: 'JSON',
+                    type: 'PUT',
+                    contentType: 'application/json',
+                    data: JSON.stringify({
+                        "user": {
+                            "id": winnerID,
+                        }
+                    })
+                };
+            },
+
         },
 
-        
+        getSelected: function(index) {
+            var filter = "input.merge_user_" + index + ":checked";   
+            checked = this.$(filter);
+            for(var i = 0; i < checked.length; i++) {
+            }            
+            return checked;            
+        },
+
+        mergeUsers: function(winnerID, mergedIDs) {
+            var that = this;
+            mergedIDs.forEach(function(mergedID, index, arr) {
+                that.ajax('mergeUsers', winnerID, mergedID).done(function() {
+                    services.notify("Merged " + mergedID + " into " + winnerID);
+                });
+            });
+        },
+
+        selectUsers: function(e) {
+            var arrID = e.currentTarget.value;
+            var winnerID = e.currentTarget.id;
+            var dupUsers = this.duplicates[arrID].users;
+
+            var checked = this.getSelected(arrID);
+
+            var merged = [];
+            var mergedIDs = [];
+
+            for(var i = 0; i < checked.length; i++) {
+                if(checked[i].id !== winnerID){
+                    merged.push(dupUsers[checked[i].value]);
+                    mergedIDs.push(checked[i].id);
+                }
+            }
+            merged.forEach(function(user, index, array) {
+                dupUsers.splice(dupUsers.indexOf(user),1);
+            });
+
+            this.mergeUsers(winnerID, mergedIDs);
+
+            this.duplicates[e.currentTarget.value].merge = false;
+            this.render();
+        },
+
+        viewMerge: function(e) {
+            this.duplicates[e.currentTarget.value].merge = true;
+            this.render();
+        },
+
+        cancel: function(e) {
+            this.duplicates[e.currentTarget.value].merge = false;
+            this.render();
+        },
+
 
         init: function() {
             this.switchTo('loading');
@@ -45,12 +115,11 @@
                     that.generateData(users).done(function(data) {
                         that.duplicates = data;
                         that.trigger('dataLoaded');
-                        console.log(data);
                     });
                 });
             });
         },
-        
+
         render: function() {
             this.switchTo('users', {userlist: this.duplicates});
         },
@@ -62,7 +131,10 @@
             return that.promise(function(done,fail) {
                 var count = 0;
                 duplicates.forEach(function(duplicate, index, array) {
-                        that.addSharingAgreements(duplicate.users).done(function(){
+                    duplicate.users.forEach(function(user) {
+                        user.duplicate_index = index;
+                    });
+                    that.addSharingAgreements(duplicate.users).done(function(){
                         count++;                    
                         if(count == duplicates.length) {
                             done(duplicates);
