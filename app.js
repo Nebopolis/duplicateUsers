7ec9c663d8d086e63e7b93ad562b3bfdb538e90c
Ben Evans <bevans@zendesk.com>
2014-10-07 14:01:18 -0500

    UI, Functionality, Design updates
    UI:
    
    - Now sorts by sharing agreement
    - Now loads list of users immediately
    - Incrementally adds the sharing agreement
    
    Design:
    
    - Added highlight for the row you have selected
    - Reworked table layout
    - Fixed translations to not refrence demo app
    
    Performance:
    
    - No longer loads all network requests for sharing agreements simultaneously
    - Immediately returns the list of duplicates before sharing agreements are added
    
    Functionality:
    
    - Added sort functionality and duplicate finding to array_utils.js which now replaces find_duplicates.js
    - Moved adding sharing agreements totally into sharing_agreements.js

--- app.js
+++ app.js
@@ -90,7 +90,11 @@
         },
 
         viewMerge: function(e) {
-            this.duplicates[e.currentTarget.value].merge = true;
+            var index = e.currentTarget.value;
+            this.duplicates.forEach(function(duplicate) {
+                duplicate.merge = false;
+            });
+            this.duplicates[index].merge = true;
             this.render();
         },
 
@@ -109,10 +113,11 @@
             this.getAll('sharing_agreements', ['sharingAgreements']).done(function(agreements) {
                 var settings = {};
                 settings.agreements = agreements;
-                that.addSharingAgreements = that.require('sharing_agreements', settings);
+                var addSharingAgreements = that.require('sharing_agreements', settings);
                 that.getAll('users', ['users']).done(function(users) {
-                    that.generateData(users).done(function(data) {
-                        that.duplicates = data;
+                    var duplicates = that.require('array_utils').duplicates(users, 'name');
+                    that.duplicates = duplicates;
+                    addSharingAgreements(duplicates).done(function() {
                         that.trigger('dataLoaded');
                     });
                 });
@@ -123,26 +128,6 @@
             this.switchTo('users', {userlist: this.duplicates});
         },
 
-        generateData: function(users) {
-            var duplicates = this.require('find_duplicates')(users, 'name');
-            var that = this;
-
-            return that.promise(function(done,fail) {
-                var count = 0;
-                duplicates.forEach(function(duplicate, index, array) {
-                    duplicate.users.forEach(function(user) {
-                        user.duplicate_index = index;
-                    });
-                    that.addSharingAgreements(duplicate.users).done(function(){
-                        count++;                    
-                        if(count == duplicates.length) {
-                            done(duplicates);
-                        }
-                    });
-                });
-
-            });
-        },
     };
 
 }());
